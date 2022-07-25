43de5db9e787576cad3231c485306b17a74fcc8c
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-25 10:34:55 -0300

    Update: Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,23 @@
+SELECT
+	titulo AS nome_musica,
+    CASE
+		WHEN titulo LIKE '%Streets%' THEN REPLACE ( titulo, 'Streets', 'Code Review')
+        WHEN titulo LIKE '%Her Own%' THEN REPLACE ( titulo, 'Her Own', 'Trybe')
+        WHEN titulo LIKE '%Inner Fire%' THEN REPLACE ( titulo, 'Inner Fire', 'Project')
+        WHEN titulo LIKE '%Silly%' THEN REPLACE ( titulo, 'Silly', 'Nice')
+        WHEN titulo LIKE '%Circus%' THEN REPLACE ( titulo, 'Circus', 'Pull Request')
+        ELSE NULL
+    END AS novo_nome
+FROM SpotifyClone.musicas
+WHERE 
+    CASE
+		WHEN titulo LIKE '%Streets%' THEN REPLACE ( titulo, 'Streets', 'Code Review')
+        WHEN titulo LIKE '%Her Own%' THEN REPLACE ( titulo, 'Her Own', 'Trybe')
+        WHEN titulo LIKE '%Inner Fire%' THEN REPLACE ( titulo, 'Inner Fire', 'Project')
+        WHEN titulo LIKE '%Silly%' THEN REPLACE ( titulo, 'Silly', 'Nice')
+        WHEN titulo LIKE '%Circus%' THEN REPLACE ( titulo, 'Circus', 'Pull Request')
+		ELSE NULL
+	END
+IS NOT NULL
+ORDER BY titulo ASC ;
+    
